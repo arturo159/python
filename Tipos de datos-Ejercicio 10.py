a5766d6207f704b2406fdaef8b791b4addc87e2d
Arturo <agorarc159@ieszaidinvergeles.org>
2025-10-09 10:08:38 +0200

    Tipos de datos-Ejercicio 10

---
+++ Tipos de datos-Ejercicio 10.py
@@ -0,0 +1,6 @@
+pesoMuñecas=75
+pesoPayasos=112
+numMuñecas=input("Dime el número de muñecas que se han vendido: ")
+numPayasos=input("Dime el número de payasos que se han vendido: ")
+pesoVenta=int(pesoMuñecas)*int(numMuñecas)+int(pesoPayasos)*int(numPayasos)
+print("El peso de tu pedido es de: ", pesoVenta, "g")
