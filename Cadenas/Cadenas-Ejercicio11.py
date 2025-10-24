328eb35ea7b8967933c090130c180f5b39cdeff8
Arturo <agorarc159@ieszaidinvergeles.org>
2025-10-24 14:34:18 +0200

    Condicionales-Ejercicio1

---
+++ Cadenas/Cadenas-Ejercicio11.py
@@ -0,0 +1,4 @@
+producto=input("Dime el nombre de un producto: ")
+precio=input("Dime el precio del producto: ")
+cantidad=input("Dime cuantas unidades del producto hay: ")
+coste=float(precio) * int(cantidad)
---
+++ Condicionales/Condicionales-Ejercicio1.py
@@ -0,0 +1,5 @@
+edad=input("Dime la edad que tienes: ")
+if int(edad) < 18:
+    print ("Eres menor de edad")
+else:
+    print ("Eres mayor de edad")
